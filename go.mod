9ceb103c96938f1cb5d29fa3bbaadd3ed21f84fe
Chen Weiguang <chen.weiguang@gmail.com>
2020-04-17 00:37:29 +0800

    feat(flag): allow allow-other with go-arg
    Optionally accepts `--allow-other` flag so that the mount is visible to
    other user other than the user.
    
    Use a declarative struct via `go-arg` to describe all the flags.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	bazil.org/fuse v0.0.0-20180421153158-65cc252bf669
+	github.com/alexflint/go-arg v1.3.0
 	github.com/google/go-github/v28 v28.1.1
 	golang.org/x/net v0.0.0-20191112182307-2180aed22343 // indirect
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
@@ -14,50 +12,55 @@ import (
 	"golang.org/x/oauth2"
 )
 
-func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s <mountpoint> <owner> <repo> [access-token]\n", os.Args[0])
-	flag.PrintDefaults()
+type args struct {
+	Mountpoint  string `arg:"positional,required"`
+	Owner       string `arg:"positional,required"`
+	Repo        string `arg:"positional,required"`
+	AccessToken string `arg:"--token" help:"GitHub access token for authorization"`
+	AllowOther  bool   `arg:"--allow-other" help:"Use FUSE allow_other mode (allow_root doesn't work, so not available)"`
+}
+
+func (args) Description() string {
+	return "GitHub Release Assets FUSE CLI"
 }
 
 func main() {
-	flag.Usage = usage
-	flag.Parse()
-
-	if flag.NArg() < 3 || flag.NArg() > 4 {
-		usage()
-		os.Exit(1)
-	}
-
-	mountpoint := flag.Arg(0)
-	owner := flag.Arg(1)
-	repo := flag.Arg(2)
-	oauth2Token := flag.Arg(3)
+	var args args
 
 	// GitHub Set-up
 	ctx := context.Background()
 
 	var tc *http.Client
-	if oauth2Token != "" {
+	if args.AccessToken != "" {
 		ts := oauth2.StaticTokenSource(
-			&oauth2.Token{AccessToken: oauth2Token},
+			&oauth2.Token{AccessToken: args.AccessToken},
 		)
 		tc = oauth2.NewClient(ctx, ts)
 	}
 
 	client := github.NewClient(tc)
-	mgmt, err := makeReleaseAssetsMgmt(ctx, client, owner, repo)
+	mgmt, err := makeReleaseAssetsMgmt(ctx, client, args.Owner, args.Repo)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// FUSE Set-up
-	c, err := fuse.Mount(
-		mountpoint,
+	mountOptions := []fuse.MountOption{
 		fuse.FSName("ghafs"),
 		fuse.Subtype("ghafs"),
 		fuse.LocalVolume(),
-		fuse.VolumeName(fmt.Sprintf("GitHub Release Assets Filesystem for %s/%s", owner, repo)),
+		fuse.VolumeName(fmt.Sprintf("GitHub Release Assets Filesystem for %s/%s", args.Owner, args.Repo)),
+		fuse.ReadOnly(),
+	}
+
+	if args.AllowOther {
+		mountOptions = append(mountOptions, fuse.AllowOther())
+	}
+
+	// FUSE Set-up
+	c, err := fuse.Mount(
+		args.Mountpoint,
+		mountOptions...,
 	)
 
 	if err != nil {
@@ -67,12 +70,7 @@ func main() {
 	defer c.Close()
 
 	// Prepare to pass around the token via reference
-	var token *string
-	if oauth2Token != "" {
-		token = &oauth2Token
-	}
-
-	err = fs.Serve(c, NewGhaFS(mgmt, token))
+	err = fs.Serve(c, NewGhaFS(mgmt, &args.AccessToken))
 	if err != nil {
 		log.Fatal(err)
 	}
